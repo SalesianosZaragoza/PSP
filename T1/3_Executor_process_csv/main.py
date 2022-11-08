f105d92037e0f92f66bf746e91263692f60c09e0
Gorka Sanz <gorka.sanz@salesianos.edu>
2022-11-08 12:54:30 +0100

    fixed errors

--- T1/3_Executor_process_csv/main.py
+++ T1/3_Executor_process_csv/main.py
@@ -2,18 +2,22 @@ from datetime import datetime
 
 from services.csv_service import CSVService
 from services.income_processor_service import IncomeProcessorService
+from services.income_processor_concurrent_service import IncomeProcessorConcurrentService
 import os
 
 if __name__ == '__main__':
-    start_time = datetime.now()
-
+    # 1. Read CSV file
     csv_service = CSVService()
     incomes = csv_service.read_csv(os.getcwd()+'/T1/3_Executor_process_csv/data.csv')
-
+    # 2. Select income Processor
     income_processor = IncomeProcessorService()
-    income_per_region = income_processor.average_per_region(incomes)
+    #income_processor = IncomeProcessorConcurrentService()
 
+    # 3. Calculate average per region
+    start_time = datetime.now()
+    income_per_region = income_processor.average_per_region(incomes)
     end_time = datetime.now()
 
+    # 4. Print results
     print(income_per_region)
     print(f'Duration: {end_time - start_time}')
--- T1/3_Executor_process_csv/services/income_processor_concurrent_service.py
+++ T1/3_Executor_process_csv/services/income_processor_concurrent_service.py
@@ -1,10 +1,10 @@
 from concurrent.futures import ProcessPoolExecutor
 import time
 from collections import defaultdict
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
-from process_csv.data_income import DataIncome
-from process_csv.income import Income
+from services.data_income import DataIncome
+from services.income import Income
 
 
 class IncomeProcessorConcurrentService:
@@ -14,17 +14,17 @@ class IncomeProcessorConcurrentService:
         for income in incomes:
             region_incomes[income.region].append(income.amount)
         
-        with ProcessPoolExecutor(max_workers=3): 
+        with ProcessPoolExecutor() as executor: 
             futures = [executor.submit(self._calculate_average, region=region, amounts=amounts) for region, amounts in region_incomes.items()]
             region_amount = {}
-            for futures in futures:   
+            for future in futures:   
                 result = future.result()
-                region_amount[region[0]] = result[1]
+                region_amount[result[0]] = result[1]
 
         return region_amount
 
     def _calculate_average(self, region: str, amounts: List[float]) -> Tuple[str, DataIncome]:
-        time.sleep(0.8)
+        #time.sleep(0.8)
         return region, DataIncome(
             average=sum(amounts) / len(amounts),
             min=min(amounts),
